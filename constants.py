a4d184ec55a43a95465d64ee49e5706ce9557ccc
d5vis <davisjbanks@gmail.com>
2024-02-07 20:51:22 -0800

    add newsletter generation and constants

---
+++ constants.py
@@ -0,0 +1,74 @@
+"""
+CONSTANTS.
+"""
+
+MESSAGES = [
+    {
+        'role': 'user',
+        'parts': ['write me a newsletter for lmu nsbe announcing the next chapter meeting', 'next meeting: ' + 'alumni panel']
+    },
+    {
+        'role': 'model',
+        'parts': ["""Hey Lions,
+
+Important announcement that tonight we’re having an…
+
+🧑🏾‍💻👨🏿‍🔧🧑🏾‍🎓 Alumni Panel (Chapter Meeting) – TONIGHT!, January 22nd @ 8pm in The Mbongi Spot
+Join us in getting to know alumni with up to decades of experience in our fields! We’ll be joined both in-person and virtually by Marvin Lawton (Math, c/o 1996), Julian Hylton (Mechanical Engineering, c/o 2011), and hopefully by a few other special guests as well! We’ll be hosting a panel to hear about their experiences and for you all to ask questions and for advice about working in the field.
+
+📸 Our Instagram: @nsbelmu
+
+That’s all; Hoping to see you all tonight!"""]
+    },
+        {
+        'role': 'user',
+        'parts': ['write me a newsletter for lmu nsbe announcing the next chapter meeting', 'next meeting: ' + 'game night']
+    },
+    {
+        'role': 'model',
+        'parts': ["""Hi Lions,
+
+Welcome back and Happy New Year! Important reminder (especially for those attending the conference) for tonight’s…
+
+🧑🏾‍💻🕹️ Game Night (Chapter Meeting) – TODAY!, January 8th @ 8pm in The Mbongi Spot
+Join us tonight for a game night to kick off the new semester and talk about what’s coming up for the rest of this year!
+
+📸 Our Instagram: @nsbelmu
+
+That’s all; see you tonight!"""]
+    },
+    {
+        'role': 'user',
+        'parts': ['write me a newsletter for lmu nsbe announcing the next chapter meeting', 'next meeting: ' + 'study jam']
+    },
+    {
+        'role': 'model',
+        'parts': ["""Hi Lions,
+ 
+Happy Monday! Hope everyone’s been pushing through the busy season. To help with that, Monday will be a…
+ 
+🧑🏾‍💻📝 Study Jam (Chapter Meeting) – Monday, November 20th @ 7:30pm in The Mbongi Spot
+Join us for a group work session where we can all be productive and support each other in our academics! Feel free to study together with your classmates and get tips and advice from the upperclassmen. Since it’s Fall break week, don’t feel too pressured to come, but we’d still like for as many people to show up to work and hang out alongside each other.
+ 
+📸 Our Instagram: @nsbelmu
+ 
+That’s all for this week; See you Monday!"""
+]
+    },
+    {
+        'role': 'user',
+        'parts': ['write me a newsletter for lmu nsbe announcing the next chapter meeting', 'next meeting: ' + 'resume workshop']
+    },
+    {
+        'role': 'model',
+        'parts': ["""Hi Lions,
+ 
+📝 Resume Workshop (Chapter Meeting) – TOMORROW, Monday, September 25th @ 7:30pm in The Mbongi Spot
+Join us for tips on resume writing and resume building from experienced students from a variety of STEM majors. We’re here to give insight and help review and perfect your resumes right in time for the start of interview season!
+ 
+📸 Our Instagram: @nsbelmu
+ 
+That’s all; See you tomorrow!"""
+    ]
+    },
+]
--- nsbelmu.py
+++ nsbelmu.py
@@ -1,8 +1,59 @@
 """
-LMU NSBE AI.
+NSBELMU AI.
 """
+import google.generativeai as genai
+import webbrowser
+from apikeys import GEMINI_API_KEY
+from constants import MESSAGES
+
+genai.configure(api_key=GEMINI_API_KEY)
 
 class NSBEAI:
-    def __init__(self, gemini_model):
-        self.model = gemini_model
+    def __init__(self):
+        safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        ]
+        self.model = genai.GenerativeModel(
+            model_name="gemini-pro",
+            safety_settings=safety_settings
+        )
 
+    def generate_newsletter(self, meeting, date):
+        """
+        Generate a newsletter for LMU NSBE announcing the next chapter meeting.
+
+        Args:
+        - meeting: The type of the next chapter meeting.
+        - date: The date of the next chapter meeting.
+
+        Returns:
+        - response: The generated newsletter.
+        """
+        prompt = "generate a newsletter for lmu nsbe announcing the next chapter meeting. try to be in a similar format to your previous responses."
+        messages = MESSAGES
+        messages.append({
+            'role': 'user',
+            'parts': [prompt,
+                      "next meeting: " + meeting,
+                      "date: " + date]
+        })
+        response = self.model.generate_content(messages)
+        text = response.text
+        mailto = f'mailto:?subject=🦁 NSBE Newsletter - {date}&body={text}'
+        webbrowser.open(mailto)
+        return text
